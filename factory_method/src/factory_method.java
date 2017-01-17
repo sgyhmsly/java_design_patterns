b231c58afd80bf48fd57de3e591f5b8692054e18
sgyhmsly <84810905@qq.com>
2017-01-17 14:37:54 +0800

    finish the abstract factory method

--- factory_method/src/factory_method.java
+++ factory_method/src/factory_method.java
@@ -1,12 +1,17 @@
-import java.util.ArrayList;
+
 
 /**
  * Created by DT173 on 2017/1/17.
  */
+import  factory_method.*;
 public class factory_method
 {
     public static void main(String args[])
     {
-       ArrayList<Integer>
+        AbstractApplication myApp1 = new Word_Application();
+        myApp1.processDoc();
+
+        AbstractApplication myApp2 = new PDF_Application();
+        myApp2.processDoc();
     }
 }
